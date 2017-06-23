14ff55e6b01eaa9a12f60dc4b6411e4534cda7f1
zykee <3349567853@qq.com>
2017-06-24 01:59:44 +0800

    test7

---
+++ main/main.js
@@ -0,0 +1,18 @@
+module.exports = function main(email, suffixes) {
+  // Write your code here
+  var e=email
+  var s=suffixes
+  var i
+  for(i=0;i<e.length;i++)
+  {
+  	if(e[i]='@')
+  		break;
+  }
+  for(var j=0;j<s.length;j++,i++)
+  {
+  	if(e[i+1]！=s[j])
+  		return false
+  }
+  else
+  	return true
+};
